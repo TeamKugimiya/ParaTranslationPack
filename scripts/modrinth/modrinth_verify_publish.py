90614e77c79e20d7f8e36eb0bb66b587c7f94b8a
Miku <26039249+xMikux@users.noreply.github.com>
2025-02-21 00:52:29 +0800

    fix: wrong date and cleaning

--- scripts/modrinth/modrinth_verify_publish.py
+++ scripts/modrinth/modrinth_verify_publish.py
@@ -2,9 +2,9 @@
 Get Modrinth release version to check is it duplicate or not.
 """
 import requests
+import pytz
 from scripts.utils import github_write_step_output
 from loguru import logger
-from pytz import timezone
 from datetime import datetime
 
 
@@ -31,12 +31,13 @@ def get_current_date() -> str:
         str:
             Current date.
     """
-    return str(timezone("Asia/Taipei").localize(datetime.now()).date())
-
+    tz = pytz.timezone("Asia/Taipei")
+    return datetime.now(tz).date()
 
 def run():
     modrinth_latest_release_date = get_modrinth_latest_publish_date()
     current_date = get_current_date()
+    logger.debug("Modrinth: {} | Current: {}", modrinth_latest_release_date, current_date) # noqa
     is_same_date = modrinth_latest_release_date == current_date
     logger.info(f"Modrinth latest release date is {'same' if is_same_date else 'not same'} as current date!") # noqa
     github_write_step_output("is_same_date", str(is_same_date).lower())
--- scripts/utils/__init__.py
+++ scripts/utils/__init__.py
@@ -1,2 +1,2 @@
-from .github import github_write_step_output
-from .utils import load_jsondata, load_tomldata, validate_json_schema
+from .github import *
+from .utils import *
