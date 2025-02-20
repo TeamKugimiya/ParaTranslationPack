b8daa5f191f31e261a3d07a1606f8c300339a9e1
Miku <26039249+xMikux@users.noreply.github.com>
2025-02-20 21:46:19 +0800

    refactor: cleanup

--- scripts/cache/cache_generate.py
+++ scripts/cache/cache_generate.py
@@ -1,20 +1,25 @@
 """
 Get ParaTranz artifact id data and write it to cache file.
 """
+import os
 from pathlib import Path
 from loguru import logger
+from paratranz_py import ParaTranz
 
-def paratranz_write_cache(artifact_data: dict, cache_path: Path, cache_fname: str) -> bool:
+
+def paratranz_write_cache(
+        artifact_data: dict, cache_path: Path, cache_fname: str
+) -> bool:
     """
     Write ParaTranz artifact id data to cache file.
-    
-    Parameters:
-    - artifact_data: dict - ParaTranz artifact data.
-    - cache_path: Path - Cache file path.
-    - cache_fname: str - Cache file name.
-    
-    Returns:
-    - bool: True if cache file is same as data, False if cache file is not same as data.
+
+    Args:
+        artifact_data (dict):
+            ParaTranz artifact data.
+        cache_path (Path):
+            Cache folder path.
+        cache_fname (str):
+            Cache file name.
     """
     cache_path = Path(cache_path)
     data_value = str(artifact_data["id"])
@@ -26,14 +31,28 @@ def paratranz_write_cache(artifact_data: dict, cache_path: Path, cache_fname: st
     if not cache_file_path.exists():
         logger.info("Cache file is missing, create one.")
         cache_file_path.write_text(data_value)
-        return False
 
     if cache_file_path.exists():
         cache_value = cache_file_path.read_text()
         if cache_value == data_value:
             logger.info("Cache file is same as data!")
-            return True
         else:
             logger.info("Cache file is not same as data, overwrite!")
             cache_file_path.write_text(data_value)
-            return False
+
+
+def run():
+    cache_folder = ".cache"
+    cache_file = "paratranz_id_cache.txt"
+    project_id = os.getenv("PARATRANZ_PROJECT_ID")
+    api_token = os.getenv("PARATRANZ_API_TOKEN")
+    para = ParaTranz(
+        api_token=api_token
+    )
+
+    artifact_data = para.artifacts.get_artifacts_info(project_id)
+    paratranz_write_cache(artifact_data, cache_folder, cache_file)
+
+
+if __name__ == "__main__":
+    run()
