08fe9bac080ace47b2d348e70a60bd244fa983bd
Miku <26039249+xMikux@users.noreply.github.com>
2025-02-21 20:31:33 +0800

    chore: implement para build

--- scripts/utils/utils.py
+++ scripts/utils/utils.py
@@ -1,7 +1,9 @@
 import json
 import tomllib
+import pytz
 from pathlib import Path
 from loguru import logger
+from datetime import datetime
 from jsonschema import validate, ValidationError
 
 
@@ -86,3 +88,76 @@ def validate_json_schema(data: dict, schema: dict) -> bool:
     except ValidationError as e:
         logger.error(f"JSON validation error: {e}")
         return False
+
+
+def convert_epoch(timestamp: str) -> int:
+    """Convert ISO timestamp to epoch time.
+
+    Args:
+        timestamp (str):
+            Timestamp.
+
+    Returns:
+        int:
+            Epoch time.
+    """
+    iso_timestamp = timestamp
+    dt = datetime.fromisoformat(iso_timestamp.replace("Z", "+00:00"))
+    epoch_time = dt.timestamp()
+    return epoch_time
+
+
+def copy_file(src: str, dest: str) -> bool:
+    """
+    Copy file from source to destination.
+
+    Args:
+        src (str): Source file path.
+        dest (str): Destination file path.
+
+    Returns:
+        bool: True if the file is copied successfully, False otherwise.
+    """
+    src = Path(src)
+    dest = Path(dest)
+    try:
+        dest.write_bytes(src.read_bytes())
+        return True
+    except FileNotFoundError as e:
+        logger.error(f"File not found: {e}")
+    except Exception as e:
+        logger.error(f"Error copying file: {e}")
+    return False
+
+
+def current_date() -> str:
+    """Get current date.
+
+    Returns:
+        str:
+            Current date.
+    """
+    tz = pytz.timezone("Asia/Taipei")
+    return datetime.now(tz).strftime("%Y/%m/%d")
+
+
+def current_time() -> str:
+    """Get current time.
+
+    Returns:
+        str:
+            Current time.
+    """
+    tz = pytz.timezone("Asia/Taipei")
+    return datetime.now(tz).strftime("%H:%M:%S")
+
+
+def current_dtime() -> str:
+    """Get current date and time.
+
+    Returns:
+        str:
+            Current date and time.
+    """
+    tz = pytz.timezone("Asia/Taipei")
+    return datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
